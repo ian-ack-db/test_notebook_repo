6690ab31d127cb03690dccbe76201cdf324ee6c4
ian-ack-db <ian-ack-db>
2024-10-18 22:15:24 +0000

    Add comment

--- parent_folder_2/repo_notebook_mlflow_ian.py
+++ parent_folder_2/repo_notebook_mlflow_ian.py
@@ -6,6 +6,7 @@
 import mlflow
 import time
 
+# Create a basic run and metric
 with mlflow.start_run() as mlflow_run:
   mlflow_client = mlflow.MlflowClient()
   mlflow_client.log_metric(mlflow_run.info.run_id, "test_metric", 0.99, step=0)
